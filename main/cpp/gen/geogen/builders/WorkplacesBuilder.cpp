32d36508667ec66f2c547f24c6319c5113ad95da
LorinTeugels <Lorin.Teugels@hotmail.com>
2018-03-13 03:44:52 +0100

    workplaces implemented

--- main/cpp/gen/geogen/builders/WorkplacesBuilder.cpp
+++ main/cpp/gen/geogen/builders/WorkplacesBuilder.cpp
@@ -10,19 +10,33 @@ using namespace std;
 using namespace util;
 using namespace trng;
 
-static shared_ptr<vector<WorkPlace>> build(GeoConfiguration& config, shared_ptr<GeoGrid> grid);
+static shared_ptr<vector<WorkPlace>> build(GeoConfiguration& config, shared_ptr<GeoGrid> grid)
 {
         auto workplaces = make_shared<vector<WorkPlace>>();
-
-        // TODO: implement
-
+        
         // Commuter data
-        util::CSV commuter_data = util::CSV(config.getcommutingFileName());
-        for (util::CSVRow& row : commuter_data) {
-        }
-        // Work fraction (Probably rename to commuteFraction?)
-        config.getWorkFraction();
+        // currently we assume there are about as many commutors leaving as there are coming in, so it balances out
+        // later we'll specify the commutors into a commuting.csv
 
+        // Work fraction (Probably rename to commuteFraction?)
+        unsigned int workforce = (unsigned int) config.getWorkFraction()*grid->getTotalPopulation();
+
+        //every workplace has 20 workers
+        unsigned int work_count =  workforce/20;
+        // Create the discrete distribution to sample from.
+        vector<double> fractions;
+        for(UrbanCenter center : *grid) {
+                fractions.push_back(double(center.population) / double(grid->getTotalPopulation()));
+        }
+
+        // The generator allows for parallelization.
+        auto rn_manager = config.getRNManager();
+        auto generator = rn_manager->GetGenerator(trng::fast_discrete_dist(fractions.begin(), fractions.end()));
+
+        // Create and map the workplaces to their samples.
+        for (unsigned int i = 0; i < work_count; i++) {
+                workplaces->push_back(WorkPlace(grid->at(generator()).coordinate));
+        }
 
         return workplaces;
 }
