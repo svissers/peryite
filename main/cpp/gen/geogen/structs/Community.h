0cb98d07b288634a6dc7350a49ccecf140986df0
svissers <stijn.vissers@student.uantwerpen.be>
2018-03-18 13:48:37 +0100

    id's are now being set

--- main/cpp/gen/geogen/structs/Community.h
+++ main/cpp/gen/geogen/structs/Community.h
@@ -7,7 +7,7 @@ namespace gen {
 struct Community
 {
         Community(unsigned int id, bool primary, util::GeoCoordinate coord)
-                : is_primary(primary), coordinate(coord) {}
+                : is_primary(primary), id(id), coordinate(coord) {}
         bool is_primary = true;
         unsigned int id;
         util::GeoCoordinate coordinate = util::GeoCoordinate();
