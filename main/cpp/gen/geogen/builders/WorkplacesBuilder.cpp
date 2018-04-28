a23fb0b825f69ed3cb0f4accf1ac10a7f6387aef
LorinTeugels <Lorin.Teugels@hotmail.com>
2018-04-28 21:47:51 +0200

    fixed the building workplace logic

--- main/cpp/gen/geogen/builders/WorkplacesBuilder.cpp
+++ main/cpp/gen/geogen/builders/WorkplacesBuilder.cpp
@@ -19,7 +19,8 @@ using namespace util;
 vector<shared_ptr<WorkPlace>> BuildWorkplaces(GenConfiguration& config, GeoGrid& grid)
 {
     auto workplaces = vector<shared_ptr<WorkPlace>>();
-    unsigned int total_population = config.GetTree().get<unsigned int>("population_size");
+    auto total_population = config.GetTree().get<unsigned int>("population_size");
+    auto work_fraction = config.GetTree().get<double>("work.work_fraction");
 
     // Calculate the relative active population for each center in the grid
     // Active population = population - commute_away + commute_towards
@@ -33,34 +34,36 @@ vector<shared_ptr<WorkPlace>> BuildWorkplaces(GenConfiguration& config, GeoGrid&
             for (size_t col_index = 0; col_index < column_count; col_index++) {
                 util::CSVRow row = *(commuting_data.begin()+row_index);
                 // When indexes are the same, commute is to the same center
+                total_commute[col_index] += stoi(row.GetValue(col_index));
+            }
+        }
+        for (size_t row_index = 0; row_index < commuting_data.size(); row_index++) {
+            for (size_t col_index = 0; col_index < column_count; col_index++) {
+                util::CSVRow row = *(commuting_data.begin() + row_index);
+                // When indexes are the same, commute is to the same center
                 if (row_index == col_index) {
                     // Commute towards
-                    relative_commute[row_index] += row.GetValue<unsigned int>(col_index);
-                }
-                else {
+                    relative_commute[row_index] += uint(row.GetValue<double>(col_index)/(double) (total_commute[row_index]) * (grid.at(row_index)->population * (3.0/4.0) * work_fraction));
+                } else {
                     // Commute away
-                    relative_commute[col_index] -= row.GetValue<unsigned int>(col_index);
+                    relative_commute[col_index] -= uint(row.GetValue<double>(col_index)/(double) (total_commute[col_index]) * (grid.at(col_index)->population * (3.0/4.0) * work_fraction));
                     // Commute towards
-                    relative_commute[row_index] += row.GetValue<unsigned int>(col_index);
+                    relative_commute[row_index] += uint(row.GetValue<double>(col_index)/(double) (total_commute[row_index]) * (grid.at(row_index)->population * (3.0/4.0) * work_fraction));
                 }
-                total_commute[col_index] += stoi(row.GetValue(col_index));
             }
         }
     }
     // Calculate the amount of workplaces, every workplace has 20 workers
-    // TODO: change total_population to total_active_population
-    double commute_fraction = config.GetTree().get<double>("work.commute_fraction");
-    // TODO: unsigned int total_active_population = ;
-    unsigned int total_commuting_actives = (unsigned int) (commute_fraction * total_population);
+    auto commute_fraction = config.GetTree().get<double>("work.commute_fraction");
+    auto total_active_population = uint(work_fraction*total_population*(3.0/4.0)); // 3/4 being the approximate amount of people in the working age range
+    auto total_commuting_actives = (unsigned int) (commute_fraction * total_active_population);
     unsigned int workplace_count =  total_commuting_actives/20;
 
     // Create the discrete distribution to sample from.
-    // TODO: change total_population to total_active_population
     vector<double> fractions;
     for(size_t i = 0; i < grid.size(); i++) {
-        // TODO: local_workforce = commute_fraction, relative, local_active;
-        int local_workforce = (relative_commute[i] / total_commute[i]) * (grid.at(i)->population);
-        fractions.push_back(double(local_workforce) / double(total_population));
+        auto local_workforce = relative_commute[i];
+        fractions.push_back(double(local_workforce) / double(total_active_population));
     }
     if (fractions.empty()) {
         return workplaces;
