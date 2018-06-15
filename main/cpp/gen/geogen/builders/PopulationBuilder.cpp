b34bf2c58694d760130c86f6868044ad030f57c6
svissers <stijn.vissers@student.uantwerpen.be>
2018-06-15 10:31:38 +0200

    trying out multiple threads in populationbuilder

--- main/cpp/gen/geogen/builders/PopulationBuilder.cpp
+++ main/cpp/gen/geogen/builders/PopulationBuilder.cpp
@@ -1,6 +1,8 @@
 #include "PopulationBuilder.h"
 #include "../../files/PopulationFile.h"
 #include "trng/fast_discrete_dist.hpp"
+#include <omp.h>
+#include <iostream>
 
 namespace stride {
 namespace gen {
@@ -16,28 +18,39 @@ void BuildPopulation(shared_ptr<Region> region, shared_ptr<Population>& populati
     auto hh_reference           = files::GetReferenceHouseholds(config);
     auto current_p_id           = region->first_person_id;
     auto pop_size = config.GetTree().get<unsigned int>("population_size");
-    auto end_of_while = current_p_id+pop_size;
-
+    // auto end_of_while = current_p_id+pop_size;
+    unsigned int toGenerate = pop_size/omp_get_num_threads();
     // Create a uniform distribution for the household reference set.
     auto rn_manager = config.GetRNManager();
     auto generator  = rn_manager->GetGenerator(trng::fast_discrete_dist(hh_reference.size()));
 
     // Build the households
     auto current_hh_id  = region->first_cps[ContactPoolType::Id::Household];
-    while (current_p_id < end_of_while) {
-            // Select a household from the reference set
-            int index           = generator();
-            auto household_ref  = hh_reference.at(index);
+    std::cout << omp_get_num_threads() << std::endl;
+#pragma omp parallel for shared(generator, hh_reference, current_hh_id, current_p_id, toGenerate, population, cout) default(none)
+    for (unsigned int i = 0; i < omp_get_num_threads(); ++i){
+        std::cout << omp_get_num_threads() << std::endl;
+        unsigned int generated = 0;
+        while (generated < toGenerate) {
+                // Select a household from the reference set
+                // #pragma omp atomic
+                int index           = generator();
+                auto household_ref  = hh_reference.at(index);
 
-            // Build the selected household
-            vector<shared_ptr<Person>> hh_persons;
-            for (unsigned int age : household_ref) {
-                population->CreatePerson(
-                    current_p_id++, age, current_hh_id,
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1
-                );
-            }
-            current_hh_id++;
+                // Build the selected household
+                // vector<shared_ptr<Person>> hh_persons;
+                for (unsigned int age : household_ref) {
+                    #pragma omp critical
+                    population->CreatePerson(
+                        current_p_id++, age, current_hh_id,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1
+                    );
+                    // ++current_p_id;
+                    ++generated;
+                }
+                #pragma omp atomic
+                current_hh_id++;
+        }
     }
     region->last_person_id = current_p_id - 1;
     region->last_cps[ContactPoolType::Id::Household] = current_hh_id-1;
