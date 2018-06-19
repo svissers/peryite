2028128c42f6781f9cf6515447e3978854c8568d
LorinTeugels <Lorin.Teugels@hotmail.com>
2018-06-19 13:23:40 +0200

    commenting code

--- main/cpp/util/StatisticsRetriever.cpp
+++ main/cpp/util/StatisticsRetriever.cpp
@@ -1,12 +1,12 @@
 #include "StatisticsRetriever.h"
 
 
-#define AMOUNTOFBANDSPOP 100
 namespace stride {
 namespace util {
 
 StatisticsRetriever::StatisticsRetriever(std::shared_ptr<Population>& pop) {
     m_sortedPopByIndex = std::vector<std::vector<unsigned int>>(AMOUNTOFBANDSPOP);
+    // Getting the maximum and minimum longitudes
     m_min_long = 90;
     m_max_long = 0;
     for(unsigned int i = 0; i < pop->size(); i ++){
@@ -19,6 +19,7 @@ StatisticsRetriever::StatisticsRetriever(std::shared_ptr<Population>& pop) {
         }
     }
     m_bandLength = (m_max_long - m_min_long )/AMOUNTOFBANDSPOP;
+    //going over the population and inserting the id/index of every person according to where they live
     for(unsigned int index = 0; index < pop->size(); index ++) {
         auto person = pop->at(index);
         for(unsigned int i = 0; i < AMOUNTOFBANDSPOP; i++) {
@@ -51,10 +52,11 @@ StatisticsRetriever::StatisticsRetriever(std::shared_ptr<Population>& pop) {
 std::tuple<unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int>
     StatisticsRetriever::GetStatisticsOfArea(std::shared_ptr<Population>& pop, util::spherical_point center, double radius) {
 
-
+    //the statistics this funditon will return
     std::tuple<unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int> returnVal;
 
-    auto radiusofBands = uint(round(radius/m_bandLength));
+    //calculating which parts of the sorted pop are possibly in range of the chosen area
+    auto radiusofBands = uint(ceil(radius/m_bandLength));
     auto band_of_center = uint(round( (center.get<1>() - m_min_long) / m_bandLength ));
     unsigned int beginband = std::max(uint(0), band_of_center+radiusofBands);
     auto endband = std::min(uint(AMOUNTOFBANDSPOP-1), band_of_center+radiusofBands);
@@ -69,7 +71,7 @@ std::tuple<unsigned int, unsigned int, unsigned int, unsigned int, unsigned int,
     unsigned int unemployedPeople = 0;
     unsigned int sickUnemployedPeople = 0;
 
-
+    //going over the chosen parts of the population,
     for(unsigned int i = beginband; i <= endband; i++){
         unsigned int misses = 0;
         for (unsigned int j : m_sortedPopByIndex[i]) {
@@ -77,7 +79,7 @@ std::tuple<unsigned int, unsigned int, unsigned int, unsigned int, unsigned int,
             auto personCoord = person.GetCoordinate();
             if(calculateDistance(personCoord, center) <= radius){
                 misses = 0;
-                //Fill in statistics!
+                //Fill in statistics
                 popInRadius++;
                 if(person.IsInPool(ContactPoolType::Id::School)){
                     if(person.GetHealth().IsInfected()){
@@ -101,9 +103,9 @@ std::tuple<unsigned int, unsigned int, unsigned int, unsigned int, unsigned int,
                     unemployedPeople++;
                 }
             }
-            else if(center.get<0>() < personCoord.get<0>()){    //stop when you are not going to find more people in radius
+            else if(center.get<0>() < personCoord.get<0>()){    // check if it got out of range of the area, so it doesn't check when all checks will be false
                 misses++;
-                if (misses >= 30){
+                if (misses >= 1000){
                     break;
                 }
             }
